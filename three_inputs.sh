147bdcdf4b95337ce134d3a91a827a6d8eead81f
Purva25Thakare <thakarepurva25@gmail.com>
2022-06-16 17:23:05 +0530

    Added:UC3

--- three_inputs.sh
+++ three_inputs.sh
@@ -6,4 +6,6 @@ num3=10
 echo "Enter the number3: " $num3
 A1=$(( num1+num2*num3 ))
 echo "The arithmatic computation is: " $A1
+A2=$(( num1*num2+num3 ))
+echo "The arithmatic computation is: " $A2
 
